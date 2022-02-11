1434f61438a1e886aae1032ed5dc598dde491899
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-11 12:48:00 -0500

    template 1-hbnb.html dynamic

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+$('document').ready(function () {
+    let checks_amenities = {};
+    // Evento change para cambiar el resultado del checkbox
+    $('INPUT[type="checkbox"]').change(function () {
+    // Si el checkbox tiene un check
+      if ($(this).is(':checked')) {
+          // Agrega al objeto checks_amenities
+          // cada atributo de amenity con su id y nombre
+        checks_amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+          // Si no tiene el check eliminalo del objeto
+        delete checks_amenities[$(this).attr('data-id')];
+      }
+      // Agrega los amenities(solo los nombres(valores))
+      // (del objeto checks_amenities), seguido de una coma
+      // dentro del tag H4 hijo de la clase amenities
+      $('.amenities H4').text(Object.values(checks_amenities).join(', '));
+    //   console.log(Object.values(checks_amenities));
+    });
+  });
